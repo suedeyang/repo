aefaa47b496ed9d0616998bd3aeb8090a0743aa3
suedeyang <suedeyang@gmail.com>
2021-12-18 10:53:30 +0800

    新增美勞教室系列

--- lhps_classtable.py
+++ lhps_classtable.py
@@ -3,7 +3,7 @@ import pandas as pd
 from PIL import Image
 import requests
 
-classrooms=["自然教室1","自然教室2","自然教室3","自然教室4","自然教室5","自然教室6","自然教室7","電腦教室1","電腦教室2","電腦教室3","語言教室1","語言教室2","表演藝術教室","音樂教室2","協同教室"]
+classrooms=["自然教室1","自然教室2","自然教室3","自然教室4","自然教室5","自然教室6","自然教室7","蝴蝶園教室","電腦教室1","電腦教室2","電腦教室3","語言教室1","語言教室2","表演藝術教室","音樂教室2","協同教室","美勞教室1","美勞教室2","美勞教室3"]
 selected_classroom=[]
 url="http://school.kh.edu.tw/view/index.php?WebID=180&MainType=103&SubType=0&MainMenuId=69026&SubMenuId=0&NowMainId=69026&NowSubId=0"
 html=pd.read_html(url,header=0)
@@ -18,9 +18,7 @@ st.sidebar.header("請選擇班級或科任老師(可多選)")
 class_1=st.sidebar.multiselect("班級",df.職稱.unique()[7:93],help="可一次選取多個班級")
 special_names=st.sidebar.multiselect("主任",df.姓名[1:5],help="可一次選取多位老師")
 names=st.sidebar.multiselect("科任老師",df.姓名[118:176],help="可一次選取多位老師")
-st.sidebar.write("教室功能做到一半還沒完全弄好")
 select_classroom = st.sidebar.multiselect("專科教室", classrooms ,help="可一次選取多間教室")
-
 combine_list=class_1+names+select_classroom+special_names
 #st.write(combine_list)
 
