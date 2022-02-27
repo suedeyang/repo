15dbffd3f2ef2b7e62f6914505f0f1db7d5289db
suedeyang <suedeyang@gmail.com>
2022-02-27 11:10:41 +0800

    st.form ?? session?

--- health_automate_form_version.py
+++ health_automate_form_version.py
@@ -22,7 +22,6 @@ body_temperature=[]
 obseravtion_time=[]
 get_hurt_places=[]
 
-status_code=0
 
 def add_to_airtable(basic_data,injured_part_result,trauma_result,Internal_Medicine_result,treat_method_result,body_temperature,obseravtion_time,get_hurt_places):
     #Python requests headers
@@ -53,92 +52,93 @@ def add_to_airtable(basic_data,injured_part_result,trauma_result,Internal_Medici
 
 
 
-form1 = st.sidebar.form("my_form1",clear_on_submit=True)
+
+form = st.form("basic_data",clear_on_submit=True)
 #st.sidebar.title("龍華國小傷病管理系統")
-form1.title("1.填寫基本資料")
-grade=form1.selectbox('年級',range(0,7))
-classes=form1.selectbox('班級',range(0,16))
-numbers=form1.selectbox('座號',range(0,35))
+form.title("1.填寫基本資料")
+grade=form.selectbox('年級',range(0,7))
+classes=form.selectbox('班級',range(0,16))
+numbers=form.selectbox('座號',range(0,35))
 basic_data=str(grade)+str(classes).zfill(2)+str(numbers).zfill(2)
+form.write(basic_data)
 #body_temperature = None
 #obseravtion_time=None
 #get_hurt_places=None
-if form1.form_submit_button("輸入完畢 下一步"):
+submit1=form.form_submit_button("Submit")
+if submit1:
     if not basic_data+"\n" in stu_list:
-        st.sidebar.error("龍華國小沒這位小朋友喔")
+        form.error("龍華國小沒這位小朋友喔")
     else:
         fp.close()
-        status_code=1
+        st.write(grade,"年",classes,"班",numbers,"號 小朋友開始登記受傷資料")
+        form2 = st.form("my_form2",clear_on_submit=True)   
+    
+        form2.header("受傷部位")
+        injured_area = form2.multiselect('',injured_part)
+        injured_part_result=[] #受傷部位結果之串列
+        for i in injured_area:
+            selected_number=injured_part.index(i)
+            injured_part_result.append(selected_number)
+        form2.write('------------')
+        form2.header("外傷")
+        trauma = form2.multiselect('',trauma_type)
+        trauma_result=[]
+        for i in trauma:
+            selected_number=trauma_type.index(i)
+            trauma_result.append(selected_number)
+        form2.write('------------')
+        form2.header("內科")
+        Internal_Medicine = form2.multiselect('',Internal_Medicine_type)
+        Internal_Medicine_result=[]
+        for i in Internal_Medicine:
+            selected_number=Internal_Medicine_type.index(i)
+            Internal_Medicine_result.append(selected_number)
+        form2.write('------------')
+        form2.header("處置作為")
+        treat_method_choice = form2.multiselect('',treat_method)
+        treat_method_result=[]
+        for i in treat_method_choice:
+            selected_number=treat_method.index(i)
+            treat_method_result.append(selected_number)
+        form2.write("-------")
+
+        if form2.form_submit_button("輸入完畢"):
+            if not injured_part_result  and not trauma_result and not Internal_Medicine_result and not treat_method_result:
+                form2.error("請先輸入基本資料與傷病資料")    
+            else:         
+            #if st.button(basic_data+"  輸入完畢 提交資料"):
+                x=add_to_airtable(basic_data,str(injured_part_result),str(trauma_result),str(Internal_Medicine_result),str(treat_method_result),str(body_temperature),str(obseravtion_time),str(get_hurt_places))
+                if x > 300:
+                    st.error("資料寫入失敗，清除資料重新登記")
+                    time.sleep(3)
+                    #pyautogui.hotkey("ctrl","F5")
+                else:
+                    st.success("資料寫入成功!!")
+                    st.balloons()
+                    time.sleep(1)
+                    #pyautogui.hotkey("ctrl","F5")
+        
+        
         #pass
-
-st.sidebar.image("https://pic.pimg.tw/c41666/1560907397-2167670633_n.png",caption='身體部位圖')
-
-'''
 with st.expander("補充資料"):
-    #colx,coly,colz=st.columns(3)
-    if st.checkbox("記錄體溫"):
+    colx,coly,colz=st.columns(3)
+    if colx.checkbox("記錄體溫"):
        body_temperature.append(st.slider("體溫",34.0,40.0,36.0,0.1))
-    if st.checkbox("紀錄休息觀察時間"):
+    if coly.checkbox("紀錄休息觀察時間"):
        obseravtion_time.append(st.selectbox("休息觀察時間",rest_time))
-    if st.checkbox("紀錄受傷地點"):       
+    if colz.checkbox("紀錄受傷地點"):       
        get_hurt_places.append(st.selectbox("受傷地點",injured_places))
 
-'''
-if status_code==1:
-    st.title("2.登記傷病資料")
-    st.success("資料驗證正確")
-    st.write(grade,"年",classes,"班",numbers,"號 小朋友開始登記受傷資料")
+
+#st.write("體溫：",body_temperature[0],)
 
 
-form2 = st.form("my_form2",clear_on_submit=True)   
-    
-form2.header("受傷部位")
-injured_area = form2.multiselect('',injured_part)
-injured_part_result=[] #受傷部位結果之串列
-for i in injured_area:
-    selected_number=injured_part.index(i)
-    injured_part_result.append(selected_number)
-form2.write('------------')
-form2.header("外傷")
-trauma = form2.multiselect('',trauma_type)
-trauma_result=[]
-for i in trauma:
-    selected_number=trauma_type.index(i)
-    trauma_result.append(selected_number)
-form2.write('------------')
-form2.header("內科")
-Internal_Medicine = form2.multiselect('',Internal_Medicine_type)
-Internal_Medicine_result=[]
-for i in Internal_Medicine:
-    selected_number=Internal_Medicine_type.index(i)
-    Internal_Medicine_result.append(selected_number)
-form2.write('------------')
-form2.header("處置作為")
-treat_method_choice = form2.multiselect('',treat_method)
-treat_method_result=[]
-for i in treat_method_choice:
-    selected_number=treat_method.index(i)
-    treat_method_result.append(selected_number)
-form2.write("-------")
 
-if form2.form_submit_button("輸入完畢"):
-    if not injured_part_result  and not trauma_result and not Internal_Medicine_result and not treat_method_result:
-        form2.error("請先輸入基本資料與傷病資料")    
-    else:         
-    #if st.button(basic_data+"  輸入完畢 提交資料"):
-        x=add_to_airtable(basic_data,str(injured_part_result),str(trauma_result),str(Internal_Medicine_result),str(treat_method_result),str(body_temperature),str(obseravtion_time),str(get_hurt_places))
-        if x > 300:
-            st.error("資料寫入失敗，清除資料重新登記")
-            time.sleep(3)
-            #pyautogui.hotkey("ctrl","F5")
-        else:
-            st.success("資料寫入成功!!")
-            st.balloons()
-            time.sleep(1)
-            #pyautogui.hotkey("ctrl","F5")
+
+
     
    
 
 
-
+st.write(basic_data)
 
