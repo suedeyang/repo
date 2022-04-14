56dab23e2d5890afdc479655e8a28f0a37dfd7b1
suedeyang <suedeyang@gmail.com>
2022-04-14 11:54:10 +0800

    Update teacher_input_with_session.py

--- teacher_input_with_session.py
+++ teacher_input_with_session.py
@@ -7,6 +7,12 @@ import requests
 #KEY=''
 #endpoint='https://api.airtable.com/v0/appCY8QfugFWkKMvH/id'
 
+alphaTable = {'A': 1, 'B': 10, 'C': 19, 'D': 28, 'E': 37, 'F': 46,
+            'G': 55, 'H': 64, 'I': 39, 'J': 73, 'K': 82, 'L': 2, 'M': 11,
+            'N': 20, 'O': 48, 'P': 29, 'Q': 38, 'R': 47, 'S': 56, 'T': 65,
+            'U': 74, 'V': 83, 'W': 21, 'X': 3, 'Y': 12, 'Z': 30}
+
+
 def add_to_airtable(id):
     #Python requests headers
     headers={
@@ -34,7 +40,7 @@ def reset_box():
             ID=df.ID_number[index_number].upper()
             with open('appendSomething.txt', 'a') as f:
                 f.write(f'{ID}\n')
-            success_message=f'{ID} 簽到成功'
+            success_message=f'{ID} 簽到成功，下一位老師請繼續簽到'
             st.success(success_message)
             st.session_state.input_box = '' #清空
         else:
@@ -43,22 +49,23 @@ def reset_box():
             st.session_state.input_box = '' #清空
     else:
         id_check=re.fullmatch("^[A-Z][12][0-9]{8}$",st.session_state.input_box.upper())
-        if id_check == None:
+        sum = alphaTable[st.session_state.input_box[0]] + int(st.session_state.input_box[1]) * 8 + int(st.session_state.input_box[2]) * 7 + int(st.session_state.input_box[3]) * 6 + int(st.session_state.input_box[4]) * 5 + int(st.session_state.input_box[5]) * 4 + int(st.session_state.input_box[6]) * 3 + int(st.session_state.input_box[7]) * 2 + int(st.session_state.input_box[8]) * 1 + int(st.session_state.input_box[9])
+        if id_check == None or sum % 10 != 0:
             st.error('身分證字號輸入錯誤，請重新輸入')
             st.session_state.input_box = '' #清空
+
         else:
             with open('appendSomething.txt', 'a') as f:
                 f.write(f'{st.session_state.input_box}\n')
-            success_message=f'{st.session_state.input_box.upper()} 簽到成功'
+            success_message=f'{st.session_state.input_box.upper()} 簽到成功，下一位老師請繼續簽到'
             st.success(success_message)
             st.session_state.input_box = '' #清空
 
 df=pd.read_csv('id.txt', sep=',')
 proxy_list=df.proxy_key.tolist()
 #st.markdown('# 龍華國小教師進修簽到 #')
-input_result=st.text_input("",max_chars=10,key='input_box',on_change=reset_box)
+input_result=st.text_input("請在下方框框中簽到",max_chars=10,key='input_box',on_change=reset_box)
 st.markdown('### **感應電梯磁扣** or **直接輸入身分證字號(輸入完畢按ENTER)** ###', unsafe_allow_html=False)
-
 
 #autofocus程式碼，要放在文字輸入框產生後再執行比較不會出錯
 components.html(
